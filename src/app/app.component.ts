21e64011747c2477c9b37fed5844ea7d63c5abca
rodvind <rod.danaei@gmail.com>
2020-02-21 15:48:31 -0700

    Add click event-binding to template and app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PassW';
+  onButtonClick() {
+    console.log('Button was clicked')
+  }
 }
