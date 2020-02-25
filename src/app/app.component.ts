af96f5dd007eb6c29724b92a625f18827ed2de8e
rodvind <rod.danaei@gmail.com>
2020-02-25 14:08:53 -0700

    Add event bindings to track user choice for checkboxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
   onButtonClick() {
     // console.log('Button was clicked')
+    console.log(`About to generate a password with the following options:
+    Includes Letters: ${this.includeLetters}
+    Includes Numbers: ${this.includeLetters}
+    Includes Symbols: ${this.includeLetters}
+    `);
     this.password = 'My PASSWORD!!!';
   }
   getPassword() {
     return this.password;
   }
+
 }
